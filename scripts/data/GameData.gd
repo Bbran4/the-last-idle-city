33671abb4279875f044b0aee6b70770005f590de
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-14 14:11:10 +0200

    Keep Scrap Yard construction cost fixed

--- scripts/data/GameData.gd
+++ scripts/data/GameData.gd
@@ -52,7 +52,7 @@ func _init() -> void:
 	materials = BigNumber.zero()
 	total_materials_produced = BigNumber.zero()
 
-	scrap_yard = ProductionOperation.new("Scrap Yard", 0.0, LEVEL_UP_BASE_COST, SCRAP_YARD_BUILD_COST, SCRAP_YARD_BUILD_ENERGY_COST, 15.0, LEVEL_UP_COST_GROWTH)
+	scrap_yard = ProductionOperation.new("Scrap Yard", 0.0, LEVEL_UP_BASE_COST, SCRAP_YARD_BUILD_COST, SCRAP_YARD_BUILD_ENERGY_COST, 1.0, LEVEL_UP_COST_GROWTH)
 	scrap_yard.level_up_energy_cost = SCRAP_YARD_LEVEL_UP_ENERGY_COST
 	scrap_yard.unlocked = true
 	scrap_yard.count = 1
