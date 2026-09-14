784764b4d14eb75a94fd0d934f9ac42ef785b202
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-14 12:32:38 +0200

    Remove obsolete A/B/C automation from game clock

--- scripts/core/GameClock.gd
+++ scripts/core/GameClock.gd
@@ -8,17 +8,13 @@ signal tick(delta: float)
 var accumulator: float = 0.0
 var running: bool = true
 
-
 func _process(delta: float) -> void:
 	if not running:
 		return
-
 	accumulator += delta
-
 	while accumulator >= tick_interval:
 		accumulator -= tick_interval
 		_process_tick(tick_interval)
-
 
 func _process_tick(delta: float) -> void:
 	GameState.data.game_time += delta
@@ -26,7 +22,5 @@ func _process_tick(delta: float) -> void:
 	GameState.data.grow_population(delta)
 	GameState.data.process_energy(delta)
 	GameState.data.process_chain_automation(delta)
-	GameState.data.process_unit_automation(delta)
 	GameState.data.produce_materials(GameState.data.scrap_yard_production_per_second().multiply_float(delta))
-
 	tick.emit(delta)
