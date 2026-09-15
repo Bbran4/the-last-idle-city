63c7fcfecb6384f5583a4ccc70dd31b05ddbd700
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-15 12:49:56 +0200

    Connect upgrade progression to main game

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 @onready var castle: Castle = $Castle
+@onready var player: Player = $Player
 @onready var wave_manager: WaveManager = $WaveManager
+@onready var upgrade_manager: UpgradeManager = $UpgradeManager
 
 func _ready() -> void:
 	GameState.start_game()
 	castle.health_changed.connect(_on_castle_health_changed)
 	_on_castle_health_changed(castle.health, castle.max_health)
+	upgrade_manager.setup(player)
 	wave_manager.setup(castle)
 	wave_manager.start()
 
