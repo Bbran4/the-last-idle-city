995191e1c027942eb70decbaae5772ca98365c9e
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-16 07:11:27 +0200

    New game idea

--- scripts/ally.gd
+++
@@ -1,9 +0,0 @@
-class_name Ally
-extends Unit
-
-@export var target: Node2D
-
-func _process(_delta: float) -> void:
-	# Ally targeting and combat are implemented after the core combat loop.
-	if target == null or is_dead:
-		return
