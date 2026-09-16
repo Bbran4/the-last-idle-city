812efc60fdc9ebd4b9682f75f3570da0be7f49ab
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-16 12:24:33 +0200

    Apply BowData visual properties to the bow scene

--- scripts/bow.gd
+++ scripts/bow.gd
@@ -6,6 +6,7 @@ extends Node2D
 const STRING_REST_X: float = 18.0
 const MAX_PULL: float = 24.0
 
+@onready var limb: Line2D = $Limb
 @onready var string_top: Line2D = $StringTop
 @onready var string_bottom: Line2D = $StringBottom
 @onready var arrow_spawn: Marker2D = $ArrowSpawn
@@ -14,7 +15,20 @@ var data: BowData
 
 func set_data(bow_data: BowData) -> void:
 	data = bow_data
+	_apply_visual_data()
 	set_draw_ratio(0.0)
+
+func _apply_visual_data() -> void:
+	if data == null:
+		return
+	limb.default_color = data.limb_color
+	limb.width = data.limb_width
+	limb.points = PackedVector2Array(0.0, -data.limb_height, 0.0, data.limb_height)
+	string_top.default_color = data.string_color
+	string_bottom.default_color = data.string_color
+	string_top.width = data.string_width
+	string_bottom.width = data.string_width
+	arrow_spawn.position.x = data.arrow_spawn_offset
 
 func set_aim(angle: float) -> void:
 	rotation = angle
