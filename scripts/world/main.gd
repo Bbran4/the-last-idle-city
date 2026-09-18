f43bef7051043d27cb7c2b1be650c95c0ecf456a
Brenden Brayshaw <brayshawbrenden@gmail.com>
2026-09-18 13:46:07 +0200

    Update organized script paths

--- scripts/world/main.gd
+++ scripts/world/main.gd
@@ -63,7 +63,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed and not event.echo:
 		match event.keycode:
 			KEY_ESCAPE:
-				get_tree().change_scene_to_file("res://scenes/hub.tscn")
+				get_tree().change_scene_to_file("res://scenes/world/hub.tscn")
 			KEY_R:
 				_reset_session()
 			KEY_E:
